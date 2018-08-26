1d1ff22efa397217c4383db90263a0dedfdb85cd
Clement Morisset <morissetcl87@gmail.com>
2018-08-26 08:53:47 +0200

    fix trouble while uploading image

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,9 +1,3 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
-
-  storage :file
-  # storage :fog
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
 end
