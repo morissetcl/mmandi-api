34051ffff0c6d2ec74e8029461b34d52f74789dc
Clement Morisset <morissetcl87@gmail.com>
2018-08-27 12:32:02 +0200

    add created_at input

--- app/admin/articles.rb
+++ app/admin/articles.rb
@@ -1,5 +1,16 @@
 ActiveAdmin.register Article do
-  permit_params :title, :summerize, :link, :image
+  permit_params :title, :summerize, :link, :image, :created_at
+
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :summerize
+      f.input :image
+      f.input :link
+      f.input :created_at, as: :date_picker
+    end
+    f.actions
+  end
 
   show do
     attributes_table do
--- app/admin/gemstories.rb
+++ app/admin/gemstories.rb
@@ -1,3 +1,14 @@
 ActiveAdmin.register Gemstory do
-  permit_params :title, :summerize, :link, :image
+  permit_params :title, :summerize, :link, :image, :created_at
+
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :summerize
+      f.input :image
+      f.input :link
+      f.input :created_at, as: :date_picker
+    end
+    f.actions
+  end
 end
