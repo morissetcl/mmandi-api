71ed5396d6be8f5f4f716d76b32bcc6734dc911f
Clement Morisset <morissetcl87@gmail.com>
2018-08-27 15:10:28 +0200

    add gem img uploader

--- app/models/gemstory.rb
+++ app/models/gemstory.rb
@@ -1,2 +1,3 @@
 class Gemstory < ApplicationRecord
+  mount_uploader :image, ImageUploader
 end
--- spec/features/gemstory_spec.rb
+++ spec/features/gemstory_spec.rb
@@ -16,4 +16,17 @@ describe 'Admin- Gemstories', type: :feature do
     expect(page).to have_content "lorem"
     expect(page).to have_content "google.fr"
   end
+
+  scenario 'Create new Gems' do
+    visit new_admin_gemstory_path
+    expect do
+      fill_in :gemstory_title, with: "In love with Rspec"
+      fill_in :gemstory_summerize, with: 'Lorem ipsum'
+      attach_file :gemstory_image, "#{Rails.root}/app/assets/images/cake.png"
+      fill_in :gemstory_link, with: 'github.com'
+      click_on 'Create Gemstory'
+    end.to change { Gemstory.count }
+    expect(page).to have_content 'github.com'
+    expect(page).to have_content "In love with Rspec"
+  end
 end
