fd2acaef80a9270eb41bae00921585ff12ba0f39
Clement Morisset <morissetcl87@gmail.com>
2018-08-31 21:50:15 +0200

    add tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180825115016) do
+ActiveRecord::Schema.define(version: 20180831192841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20180825115016) do
     t.string   "link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "tags"
   end
 
   create_table "gemstories", force: :cascade do |t|
