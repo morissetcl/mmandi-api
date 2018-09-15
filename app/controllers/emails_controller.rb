5a63dc1490ac7670b5397228c5fee90905030c5e
Clement Morisset <morissetcl87@gmail.com>
2018-09-15 22:25:47 +0200

    save data from front

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -6,11 +6,12 @@ class EmailsController < ApiController
   end
 
   def create
-    @email = Email.new(email_params)
+    @email = Email.new(email: params['data']['attributes']['email'], content: params['data']['attributes']['content'])
     if @email.save
       render json: { email: @email }, status: :created, location: @email
     else
       render json: @email.errors, status: :unprocessable_entity
     end
   end
+
 end
